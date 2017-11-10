89388daf5c2cebaf17e906e3f385f03cec50c141
tedere93 <tedere93@users.noreply.github.com>
2017-11-10 12:28:29 +0200

    regex & not empty input
    pentru formulare

--- AdminApp/functions/marketing/newsletter.php
+++ AdminApp/functions/marketing/newsletter.php
@@ -13,7 +13,7 @@
                 <!-- Check all button -->
 
                 <div class="form-group">
-                  <label>Titlul mesajului</label>
+                  <label>Titlul mesajului</label> <p id="iscorectTitlu" style="float:right">Campul nu poate fi gol!</p>
                   <input class="form-control" id="titlu-news" placeholder="" type="text">
                 </div>
                  <div class="form-group">
@@ -24,7 +24,11 @@
                 </div>  
                 <!-- /.pull-right -->
               </div>
-                <button style="width:200px;" id="trimite-buton" type="button" class="btn btn-block btn-default">Default</button>
+<<<<<<< HEAD
+                <button style="width:200px;" id="trimite-buton" type="button" class="btn btn-block btn-default">Trimite</button>
+=======
+                <input  style="width:200px;" id="trimite-buton" type="button" class="btn btn-block btn-default" value="Trimite newsletter">
+>>>>>>> 65111275b4e066608e2c08e9d6b09c0c7e4310c2
             
                                      
                                 
@@ -32,21 +36,112 @@
             </div>
             <!-- /.box-body -->
             <div class="box-footer no-padding">
-            
             </div>
           </div>
     </div>
 </div>
+<div id="dialog-newsletter" title="Confirmare newsletter">
+    <p><span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span><p id="set_nr_val">Atentie! Se va trimite mail la toti clienti inregistrați!
+</div> 
 
 <script>
+
+  
+
+  $("#editorrrul").click(function(){
+    alert(CKEDITOR.instances.editor1.getData());
+  });
+
+ 
+
+  $("#titlu-news").keyup(function(){
+    checkifDenumireProdus("trimite-buton","iscorectTitlu","titlu-news");
+
+  });
+
     
-    $('#trimite-buton').click(function(){
-        alert($('#titlu-news').val()+"    "+$('#editor1').val());
-    });
+<<<<<<< HEAD
+  $('#trimite-buton').click(function(){
+    var var1 = checkifDenumireProdus("trimite-buton","iscorectTitlu","titlu-news");
+    var var2 = 0;
+    if(var1 == 0){
+        console.log("Titlul nu pote fi gol!");
+      }else if(var1 = 1){
+        if(CKEDITOR.instances.editor1.getData()==""){
+          console.log("Mesajul nu poate fi gol!");
+        }else{
+            console.log("trimite mesaj cu textul: " + CKEDITOR.instances.editor1.getData() + "si titlul: " + $("#titlu-news").val());
+        }
+  }
+  });
+=======
+   
+>>>>>>> 65111275b4e066608e2c08e9d6b09c0c7e4310c2
  $(function () {
     // Replace the <textarea id="editor1"> with a CKEditor
     // instance, using default configuration.
+     $("#dialog-newsletter").dialog({
+      autoOpen: false,
+      modal: true
+    });
     CKEDITOR.replace('editor1');
     });
+<<<<<<< HEAD
     //bootstrap WYSIHTML5 - text editor
     </script>
+=======
+     $('#trimite-buton').click(function(){
+         
+             $( "#dialog-newsletter" ).dialog({
+			resizable: false,
+			height: "auto",
+			width: 400,
+			modal: true,
+                        autoOpen: true,
+			buttons: {
+				"Confirmare": function(e) {
+                                   
+					
+                                            $.ajax({
+            type: 'POST',
+            url: 'functions/phpMailer/mail_clienti.php',
+            data:{"message":CKEDITOR.instances['editor1'].getData(),"title":$('#titlu-news').val()},
+           beforeSend: function() { 
+                $("#dialog-newsletter").dialog( "close" );
+                },
+                success: function(data) {
+                alert(data);
+               
+                alert(data);
+               
+                 },
+                error: function(xhr, status, error) {
+  //alert(xhr.responseText);
+},
+                complete: function(data) {
+              //  alert(data);
+              
+    },
+                dataType: 'html'
+});
+                                     //   alert("dada");
+                                      
+				},
+				Anulare: function() {
+					$( this ).dialog( "close" );
+				}
+                                
+			}
+		});
+         
+         
+         
+         
+         
+   
+         return false;
+       
+    });
+ 
+    </script>
+>>>>>>> 65111275b4e066608e2c08e9d6b09c0c7e4310c2
---
+++ AdminApp/functions/phpMailer/mail_clienti.php
@@ -0,0 +1,83 @@
+<?php
+	include '../database/databaseConnection.php';	
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	
+	require 'classes/PHPMailer/Exception.php';
+	require 'classes/PHPMailer/PHPMailer.php';
+	require 'classes/PHPMailer/SMTP.php';
+	$delay=0.5;
+		mailClienti($_POST['title'],$_POST['message'],$conn);
+               $varMail="";
+               $nrClienti;
+function mailClienti($subject, $message,$mysqli){
+		
+		//$message = date("Y-m-d h:i:s").": S-a trimis mail cătrespecializarea ".$cod_specializare." de către utilizatorul cu id-ul ". $_SESSION['id']." de la următoarea adresă: ".$_SERVER['REMOTE_ADDR']."\r\n";
+		$myfile = file_put_contents('logs/good_log.txt', $message.PHP_EOL , FILE_APPEND | LOCK_EX);
+		
+			//$quantity = 2;
+		//	$delay = 2.5;
+		
+	
+//			$sql = $mysqli->query("SELECT email FROM absolventi WHERE email!='' AND cod_specializare = $cod_specializare".";");	
+//			if(mysqli_num_rows($sql)>0) {
+//				$row = mysqli_fetch_array($sql);
+//				$subject = $row['titlu'];
+//				$message = $row['continut'];
+//			}
+			
+			//$sql = $mysqli->query('SELECT email FROM absolventi WHERE email!="" and cod_specializare='.$cod_specializare.';');
+			$sql = $mysqli->query('SELECT email from user WHERE email!=""');
+                        $nrClienti= mysqli_num_rows($sql);
+			if($nrClienti) {
+			
+				$stack = array();
+				$group = 0;
+				
+				while($row = mysqli_fetch_array($sql)) {
+                                   
+                                   
+					if(filter_var($row['email'], FILTER_VALIDATE_EMAIL)) {
+						  $varMail=$row['email'];
+					
+							$mail = new PHPMailer();
+							$mail->SMTPDebug = 3; 
+							$mail->Debugoutput = 'angajatori_log';
+							$mail->IsSMTP();
+							$mail->Host = 'mail.debbies.ro'; 
+							$mail->SMTPAuth = true;
+							$mail->Username = 'newsletter@debbies.ro'; 
+							$mail->Password = 'ndhe12022208'; 
+							$mail->SMTPSecure = 'ssl'; 
+							$mail->Port = 465; 
+					    		$mail->setFrom('newsletter@debbies.ro','Noutati Debbie\'s Yogurt');
+					    		$mail->addAddress($varMail);
+					    		foreach ($stack as &$current_mail) {
+					    			$mail->AddBCC($current_mail);
+					    			echo $current_mail."-----------------------<br>";
+					    		}
+					    		$mail->isHTML(true);
+					    		$mail->Subject = $subject;
+			    				$mail->Body    = $message;
+					    		$mail->AltBody = 'EROARE: Incercati vizualizarea acestui mail folosind un alt dispozitiv.';
+					  
+						    	if(!$mail->send()) 
+						    	{
+						    		echo 'Newsletter-ul nu a putut fi trimis.</a>';
+						    		
+						    	}  else {
+						    		echo 'Mail trimis cu succes la '.$nrClienti." clienti!";
+						    	}
+	
+						    	sleep($delay);
+						    
+						
+					}
+					
+				}
+				
+			}
+                        mysqli_close($mysqli);
+		}
+	
+?>
